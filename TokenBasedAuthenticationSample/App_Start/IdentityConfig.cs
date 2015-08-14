5fac8759bb6fb0434908700e15fca9f4a1d304ab
samfgl <samfgl@hotmail.com>
2015-08-15 03:40:29 +0430

    Configuring Idenity and OWIN

---
+++ TokenBasedAuthenticationSample/App_Start/IdentityConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TokenBasedAuthenticationSample.Models;
+using TokenBasedAuthenticationSample.Services;
+
+namespace TokenBasedAuthenticationSample
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(() => new UserManager<AppUser>(new DummyUserStore()));
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/login"),
+            });
+        }
+    }
+}
