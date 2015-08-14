1672c508d60ed9db406d1b3b3422c96f5eaeb519
samfgl <samfgl@hotmail.com>
2015-08-15 04:14:51 +0430

    Adding editors only action

--- TokenBasedAuthenticationSample/Controllers/TopSecretController.cs
+++ TokenBasedAuthenticationSample/Controllers/TopSecretController.cs
@@ -6,10 +6,18 @@ using System.Web.Mvc;
 
 namespace TokenBasedAuthenticationSample.Controllers
 {
-    [Authorize]
+    
     public class TopSecretController : Controller
     {
+        [Authorize]
         public ActionResult Index()
+        {
+            var userName = User.Identity.Name;
+            return View((object)userName);
+        }
+
+        [Authorize(Roles ="Editor")]
+        public ActionResult EditorsOnly()
         {
             var userName = User.Identity.Name;
             return View((object)userName);
